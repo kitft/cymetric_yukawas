490528c0c26190d89afbae9ccdc983e412df1467
Cristofero Fraser-Taliente <58341426+kitft@users.noreply.github.com>
2025-02-20 15:52:02 +0000

    latest

--- yukawas/run_model13.sh
+++ yukawas/run_model13.sh
@@ -1,3 +1,3 @@
 echo Running the following settings: $1 $2 $3
 echo \n
-/mnt/users/kitft/cymetricTESTING216/bin/python -u Model_13_Do.py $1 $2 $3
+~/cymetric-yukawas/.venv/bin/python -u Model_13_Do.py $1 $2 $3
