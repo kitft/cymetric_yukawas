b48104fb3c89599c3deb8b72b3265ecaa3b88dd9
Cristofero Fraser-Taliente <58341426+kitft@users.noreply.github.com>
2025-02-24 12:27:30 +0000

    add yukawas as an import

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/kitft/cymetric_yukawas",
-    packages=setuptools.find_packages(),
+    packages=['cymetric', 'AlphaPrime', 'yukawas'] + setuptools.find_packages(include=['cymetric.*', 'AlphaPrime.*','yukawas.*']),#packages=['cymetric', 'AlphaPrime'] + setuptools.find_packages(include=['cymetric.*', 'AlphaPrime.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
