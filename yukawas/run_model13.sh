17efe2516ac2b7a681a39335ff839b17b2fd9c59
Cristofero Fraser-Taliente <58341426+kitft@users.noreply.github.com>
2025-02-26 01:59:08 +0000

    current

--- yukawas/run_model13.sh
+++ yukawas/run_model13.sh
@@ -1,3 +1,2 @@
 echo Running with arguments: "$@"
-echo \n
 ~/cymetric_yukawas/.venv/bin/python -u Model_13_Do.py "$@"
--- yukawas/run_testint.sh
+++ yukawas/run_testint.sh
@@ -1,3 +1,2 @@
 echo Running with arguments: "$@"
-echo \n
 ~/cymetric_yukawas/.venv/bin/python -u test_integration.py "$@"
