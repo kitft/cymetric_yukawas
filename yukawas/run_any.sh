c656afefd99e9c64aa251cb2ccf903803f02602b
Cristofero Fraser-Taliente <58341426+kitft@users.noreply.github.com>
2025-03-02 20:00:49 +0000

    new logs

--- yukawas/run_any.sh
+++ yukawas/run_any.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --output=runmodel_%1_%2_%j_%3.out
+#SBATCH --output=logs/runmodel_%1_%2_%j_%3.out
 
 echo Running with arguments: "$@"
 ~/cymetric_yukawas/.venv/bin/python -u do_model.py "$@" "$SLURM_JOB_ID"
