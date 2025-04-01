77a1796317d69e69f587551ac85a5a87e5bb64b6
Cristofero Fraser-Taliente <58341426+kitft@users.noreply.github.com>
2025-04-01 03:18:30 +0100

    hi

--- yukawas/do_model.py
+++ yukawas/do_model.py
@@ -719,6 +719,7 @@ d, 0, c, 0, b, 0, a, 0, b, 0, c, 0, d, 0, e, 0, f, \
 0, g, 0, h, 0, i, 0, j, 0, k, 0, l, 0, m, 0, n, 0, \
 o, 0, p, 0, q, 0, r, 0, s, 0, t, 0, u])
     free_coefficient_str = str(hash(tuple(get_coefficients_here(free_coefficient))))
+    type_folder+='_random'
 
 def purge_dicts_and_mem():
     delete_all_dicts_except('dataEval','manifold_name_and_data', 'phimodel_config', 'betamodel_config', 'sigmamodel_config', 'sigma2model_config')
